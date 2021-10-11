40452c570340e133c49f0d9642634b1f804829d1
Alexander Christensen <alex_c007@hotmail.com>
2021-10-11 15:33:10 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,51 +3,9 @@
 #include "program_args.h"
 
 #define APPNAME "cmop"
+#include "main_helper.h"
 
-const char* cyclic_dependency() {
-	return "Cyclic module dependency detected. Aborting build script!";
-}
 
-const char* invalid_module_syntax() {
-	return "Invalid module syntax. Failed to generate build script!";
-}
-
-void print_error(const char* err)
-{
-	fprintf(stderr, "%s. Failed to generate build script!\n", err);
-}
-
-void print_usage_pattern()
-{
-	printf("Usage: %s [OPTION]... PATTERNS\n", APPNAME);
-}
-
-void print_usage()
-{
-	print_usage_pattern();
-	printf("Try '%s --help' for more information.\n", APPNAME);
-}
-
-void print_help()
-{
-	print_usage_pattern();
-	printf("Generate build scripts for C++20 projects that use modules.\n");
-	printf("Example: %s ", APPNAME);
-	printf("--files='src/**/*.cpp' ");
-	printf("--compiler='g++-11' ");
-	printf("--compiler-flags='' ");
-	printf("--output='stdout' ");
-	printf("\n");
-	printf("Options:\n");
-	printf("  --files='PATTERN'          include files matching PATTERN\n");
-	printf("  --compiler=VALUE           VALUE is name of compiler executable\n");
-	printf("  --compiler-flags='PATTERN' PATTERN is flags for the compiler\n");
-	printf("  --output=FILE              if this flag is specified, output build\n");
-	printf("                             commands to FILE insted of standard output\n");
-	printf("\n");
-	printf("Report bugs by opening an issue on github:\n");
-	printf("<https://github.com/alexpanter/cpp_module_parser/issues>\n");
-}
 
 int main(int argc, char* argv[])
 {
@@ -63,6 +21,11 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 	printf("argv[1]: \"%s\"\n", argv[1]);
+	
+	// read command-line args
+	program_args_t args;
+	program_args_init(&args);
+	program_args_parse(argc, argv, args);
 
 	// BUILD ORDER:
 	// 1) build headers
